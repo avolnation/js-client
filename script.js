9b9113e6cb43693f59202913555c03d4cc6d03f3
Igaresha <igorok.201367@gmail.com>
2022-03-29 00:15:38 +0300

    List of all posts, button modify post, modal window

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("btn");
+var span = document.getElementsByClassName("close")[0];
 
+function openModal(id) {
+    document.querySelector(".postBody").innerHTML = data[id-1].body
+    modal.style.display = "block";
+  }
+  
+  span.onclick = function() {
+    modal.style.display = "none";
 
-let btn = document.getElementById("info")
+  }
 
-// btn.addEventListener("click", )
-
-// btn.append()
+  document.getElementById('MyForm').addEventListener('submit', function (e) {
+    e.preventDefault()
+  
+    var inps = document.querySelectorAll("input, select, textarea")
+    
+    for (var q=0; q<inps.length; ++q) {
+      if (inps[q].name && inps[q].form === this) {
+        console.log("%s %s", inps[q].name, inps[q].value)
+      }
+    }
+  })
+  
+  document.addEventListener('click', function(e) {
+    console.log(e.target.id);
+  })
 
 
 async function fetchData(type) {
@@ -20,9 +42,19 @@ async function fetchData(type) {
     }
 }
 
-async function renderData(id) {
-    const data = await fetchData(id);;
-    console.log(data);
-}
+var data;
 
-renderData('posts');
+async function render(){
+    typeof(data) !== 'undefined' ? render() : data = await fetchData('posts');
+    console.log(data)
+    data.forEach(el => {
+        document.querySelector("#info").insertAdjacentHTML('afterbegin', 
+            `<div class="post">
+                <div class="title">${el.title}</div>
+                <div id="postBody">${el.body}</div>
+                </div>
+                <button onclick="openModal(${el.id})" class="btn" id="${el.id}">Редактировать пост</button>
+                <hr>`
+                );
+        });
+}
